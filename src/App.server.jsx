56454254adcf9158e1e9902e75e045858a1af892
Susanne Peng <susanne.peng@gmail.com>
2022-05-31 10:47:29 +1000

    Initial commit

---
+++ src/App.server.jsx
@@ -0,0 +1,37 @@
+import renderHydrogen from '@shopify/hydrogen/entry-server';
+import {
+  Router,
+  Route,
+  FileRoutes,
+  ShopifyProvider,
+  PerformanceMetrics,
+  PerformanceMetricsDebug,
+} from '@shopify/hydrogen';
+import {Suspense} from 'react';
+import DefaultSeo from './components/DefaultSeo.server';
+import NotFound from './components/NotFound.server';
+import LoadingFallback from './components/LoadingFallback';
+import CartProvider from './components/CartProvider.client';
+import {ThirdPartyServerComponent} from 'third-party-component-library';
+
+function App() {
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      <ShopifyProvider>
+        <ThirdPartyServerComponent>
+          <CartProvider>
+            <DefaultSeo />
+            <Router>
+              <FileRoutes />
+              <Route path="*" page={<NotFound />} />
+            </Router>
+          </CartProvider>
+        </ThirdPartyServerComponent>
+        <PerformanceMetrics />
+        {process.env.LOCAL_DEV && <PerformanceMetricsDebug />}
+      </ShopifyProvider>
+    </Suspense>
+  );
+}
+
+export default renderHydrogen(App);
