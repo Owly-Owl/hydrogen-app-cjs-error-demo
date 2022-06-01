1386b1d77a45174a266ac1cdc05bb5f1bf75a912
Susanne Peng <susanne.peng@gmail.com>
2022-06-01 23:27:44 +1000

    Use shopify-3p-component-library build output at SHA c468e02601010da31bc50740b365fcde987d0a29; Update vite.config.js to pre-bundle dependency of shopify-3p-component-library

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [hydrogen()],
-  optimizeDeps: {include: ['@headlessui/react']},
+  optimizeDeps: {
+    include: [
+      '@headlessui/react',
+      'third-party-component-library > html-react-parser',
+    ],
+  },
   test: {
     globals: true,
     testTimeout: 10000,
